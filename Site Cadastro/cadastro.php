65bd879011f3f719dc8ac24c44f19b0d6c20fcc0
Rebeca Lima <becaln.ti0@gmail.com>
2025-09-16 22:22:45 -0300

    Update cadastro.php
    Aula 16/09.

--- Site Cadastro/cadastro.php
+++ Site Cadastro/cadastro.php
@@ -21,15 +21,49 @@ include_once("db/conexao.php");
 //     // echo ($nome)
 
 
-// VERIFICAR SE FOI FEITO UM POST
+
 $erro = "";
+$mensagem = "";
+$id = "";
+$nome = "";
+$nascimento = "";
+$email = "";
+$login = "";
+$senha = "";
+
+// VERIFICAR SE TEM UM ID NO GET, SE TIVER, BUSCAR OS DADOS DO USUÁRIO
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+    if (!is_numeric($id)) {
+        $erro = "ID inválido, não fique mexendo nele seu salafrário.";
+    }
+    else {
+        $sql = "SELECT * FROM usuarios WHERE id = ?";
+        $stmt = $conexao->prepare($sql);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $resultado = $stmt->get_result();
+        if ($resultado->num_rows == 1) {
+            $linha = $resultado->fetch_object();
+            $nome = $linha->nome;
+            $nascimento = $linha->nascimento;
+            $email = $linha->email;
+            $login = $linha->login;
+            
+        }
+        else {
+            $erro = "Usuário não encontrado!";
+        }
+    }
+}
+
+// VERIFICAR SE FOI FEITO UM POST
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $nome = $_POST["nome"] ?? "";
     $nascimento = $_POST["nascimento"] ?? "";
     $email = $_POST["email"] ?? "";
     $login = $_POST["login"] ?? "";
     $senha = $_POST["senha"] ?? "";
-    echo("$nome - $nascimento - $email - $login - $senha");
 
     if (strlen($nome) < 5) {
         $erro .= "Seu nome deve conter no mínimo 5 dígitos";
@@ -42,7 +76,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $sql = "INSERT INTO usuarios (nome, nascimento, email, login, senha) VALUES (?, ?, ?, ?, ?)";
     $stmt = $conexao->prepare($sql);
     $stmt->bind_param("sssss", $nome, $nascimento, $email, $login, $senha);
-    $stmt->execute();
+    if($stmt->execute() === true) {
+        $id = $stmt->insert_id;
+        $mensagem = "Usuário cadastrado com sucesso!";
+    }
+    else {
+        $erro .= "Erro ao cadastrar o usuário: " .$stmt->error;
+    }
 }
 
 ?>
@@ -63,17 +103,31 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         <!-- Barra de título -->
         
         <!-- Formulário de Cadastro -->
-        
+        <?php
+            if($mensagem != "") {
+                echo ("<div class='alert alert-success my-3' role='alert'>$mensagem</div>");
+            }
+
+            if($erro != "") {
+                echo ("<div class='alert alert-danger my-3' role='alert'>$erro</div>");
+            }
+        ?>
         <div class="card shadow rounded-1 bg-body-secondary my-4">
             <div class="card-header text-center">
                 <h3 class="p-3 fw-bold">Cadastro de Usuário</h3>
             </div>
+            
             <div class="card-body">
                 <form method="post">
-                    <!-- nome -->
+                    <!-- Mostrar o ID -->
+                    <div class="mb-3">
+                        <label class="form-label">ID do usuário:</label>
+                        <input type="text" class="form-control" readonly id="codigo" name="codigo" value="<?php echo($id) ?>">
+                    </div>
+                    <!-- Nome -->
                     <div class="mb-3">
                         <label class="form-label" for="">Informe o seu nome:</label>
-                        <input id="nome" class="form-control" type="text" name="nome" required minlength="5" maxlength="250">
+                        <input id="nome" class="form-control" type="text" name="nome" required minlength="5" maxlength="250" value="<?php echo($nome) ?>">
                         <div class="invalid-feedback">
                             Informe um nome válido
                         </div>
@@ -81,31 +135,31 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                     <!-- Data Nasc -->
                     <div class="mb-3">
                         <label for="nascimento" class="form-label">Data de Nascimento:</label>
-                        <input type="date" class="form-control" id="nascimento" name="nascimento" required>
+                        <input type="date" class="form-control" id="nascimento" name="nascimento" min="1900-01-01" required value="<?php echo($nascimento) ?>">
                         <div class="invalid-feedback">
                             Informe a data de nascimento
                         </div>
                     </div>
-                    <!-- email -->
+                    <!-- Email -->
                     <div class="mb-3">
                         <label for="email" class="form-label">E-mail:</label>
-                        <input type="email" class="form-control" id="email" name="email" required>
+                        <input type="email" class="form-control" id="email" name="email" minlength="15" maxlength="100" required value="<?php echo($email) ?>">
                         <div class="invalid-feedback">
                             Informe um email valido
                         </div>
                     </div>
-                    <!-- login -->
+                    <!-- Login -->
                     <div class="mb-3">
                         <label for="login" class="form-label">Login:</label>
-                        <input type="text" class="form-control" id="login" name="login" required>
+                        <input type="text" class="form-control" id="login" name="login" minlength="4" maxlength="20" required value="<?php echo($login) ?>">
                         <div class="invalid-feedback">
                             Informe um login
                         </div>
                     </div>
-                    <!-- senha -->
+                    <!-- Senha -->
                     <div class="mb-3">
                         <label for="senha" class="form-label">Senha:</label>
-                        <input type="password" class="form-control" id="senha" name="senha" required minlength="8" maxlength="16">
+                        <input type="password" class="form-control" id="senha" name="senha" required minlength="8" maxlength="16" value="<?php echo($senha) ?>">
                         <div class="invalid-feedback">
                             Informe uma senha válida
                         </div>
